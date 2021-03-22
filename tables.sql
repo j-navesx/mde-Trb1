f47bc33c51360c4066e7a99732b982933469770e
j-navesx <j.naves@campus.fct.unl.pt>
2021-03-22 11:06:28 +0000

    Updates on tables
    Error fixing and such

--- tables.sql
+++ tables.sql
@@ -1,13 +1,20 @@
-CREATE TABLE User(
-    uuid primary key,
+CREATE TABLE fUser(
+    id integer,
     name varchar2(255),
     bday date,
-    weight float,
-    height float,
-    premium boolean,
-    pt_status boolean,
-    active boolean,
-    my_activities foreign primary key, 
-    my_pt foreign primary key,
-    my_transactions foreign primary key
+    weight float(126),
+    height float(126),
+    premium number(1),
+    pt_status number(1),
+    active number(1)
 );
+
+
+/*
+ALTER TABLE Fitness_User
+ADD my_activities integer references activities_rel(id) 
+ADD my_pt integer references pts_rel(id)
+ADD my_transactions integer references transactions_rel(id);
+*/
+
+SELECT * FROM fUser;
