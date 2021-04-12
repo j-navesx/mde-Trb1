082a20d5d2ad0d447965bbfa49979ff4b9ac3b08
j-navesx <j.naves@campus.fct.unl.pt>
2021-04-12 11:31:08 +0100

    Model Changes
    Fitness Database Model updated

--- Fitness/DBconnect.java
+++
@@ -1,9 +0,0 @@
-public class DBconnect{
-    static{
-        System.load("c:\\")
-    }
-    static synchronized native public void testfunc();
-    public static void main(){
-        DBconnect.testfunc();
-    }
-}
