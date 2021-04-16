f24f394a6508733dec863d14283c0dda47d83504
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-04-16 18:43:35 +0100

    Error Correction.
    user and profile creation.

--- querrys/hardcoded/create_user.sql
+++ querrys/hardcoded/create_user.sql
@@ -1,8 +1,26 @@
 insert into fit_user (username, password, email, premium, active, pt)
 values ('comrad','russia','viktor@kgb.com',1,1,0);
 
+INSERT INTO profile (name,weight,height,bday,gender) 
+VALUES ('comrad',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
+
 insert into fit_user (username, password, email, premium, active, pt)
 values ('comrad2','russia','viktor2@kgb.com',1,1,1);
 
+INSERT INTO profile (name,weight,height,bday,gender) 
+VALUES ('comrad2',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
+
 insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
 values ('comrad3','russia','viktor3@kgb.com',1,1,0,2);
+
+INSERT INTO profile (name,weight,height,bday,gender) 
+VALUES ('comrad3',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
+
+insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
+values ('comrad4','russia','viktor4@kgb.com',1,1,0,2);
+
+INSERT INTO profile (name,weight,height,bday,gender) 
+VALUES ('comrad4',100,250,to_date('16/04/2015','DD/MM/YYYY'),'male');
+
+select * from fit_user;
+select * from profile;
