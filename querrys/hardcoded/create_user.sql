1ea6fb11b7075493f0bdb0bfc25381659008d797
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-04-16 19:33:55 +0100

    Create user and profile
    Procidure for create user ans profile functionality done!

--- querrys/hardcoded/create_user.sql
+++ querrys/hardcoded/create_user.sql
@@ -1,28 +1,36 @@
 
-insert into fit_user (username, password, email, premium, active, pt)
-values ('comrad','russia','viktor@kgb.com',1,1,0);
-
-insert into profile (name,weight,height,bday,gender) 
-VALUES ('comrad',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
-
-insert into fit_user (username, password, email, premium, active, pt)
-values ('comrad2','russia','viktor2@kgb.com',1,1,1);
-
-insert into profile (name,weight,height,bday,gender) 
-VALUES ('comrad2',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
-
-insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
-values ('comrad3','russia','viktor3@kgb.com',1,1,0,2);
-
-insert into profile (name,weight,height,bday,gender) 
-VALUES ('comrad3',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
-
-insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
-values ('comrad4','russia','viktor4@kgb.com',1,1,0,2);
-
-insert into profile (name,weight,height,bday,gender) 
-VALUES ('comrad4',100,250,to_date('16/04/2015','DD/MM/YYYY'),'male');
+create or replace procedure create_user(
+    iusername     VARCHAR2,
+    ipassword     VARCHAR2,
+    iemail        VARCHAR2,
+    ipremium      NUMBER,
+    iactive       NUMBER,
+    ipt           NUMBER,
+    ipt_id        INTEGER,
+    iname         VARCHAR2,
+    iweight       NUMBER,
+    iheight       NUMBER,
+    ibday         DATE,
+    igender       VARCHAR2
+) as
+begin
+    if ipt_id != null then
+        insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
+        values (iusername,ipassword,iemail,ipremium,iactive,ipt,ipt_id);
+        
+        insert into profile (name, weight, height, bday, gender)
+        values (iname, iweight, iheight, ibday, igender);
+    else
+        insert into fit_user (username, password, email, premium, active, pt)
+        values (iusername,ipassword,iemail,ipremium,iactive, ipt);
+        
+        insert into profile (name, weight, height, bday, gender)
+        values (iname, iweight, iheight, ibday, igender);
+    end if;
+end;
 
 select * from fit_user;
 select * from profile;
+
+
 
--- querrys/read/read_user.sql
+++ querrys/read/read_user.sql
@@ -11,8 +11,14 @@ begin
     if ipt_id != null then
         insert into fit_user (username, password, email, premium, active, pt, fit_user_id)
         values (iusername,ipassword,iemail,ipremium,iactive,ipt,ipt_id);
+        
+        insert into profile (name, weight, height, bday, gender)
+        values ('comrad',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
     else
         insert into fit_user (username, password, email, premium, active, pt)
         values (iusername,ipassword,iemail,ipremium,iactive, ipt);
+        
+        insert into profile (name, weight, height, bday, gender)
+        values ('comrad',100,250,to_date('16/04/2000','DD/MM/YYYY'),'male');
     end if;
 end;
