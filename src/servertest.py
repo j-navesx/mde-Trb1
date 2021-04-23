f394959a7329dbf79c5790e14ce874de7e61c738
j-navesx <j.naves@campus.fct.unl.pt>
2021-04-23 23:40:45 +0100

    Quality of life changes + POST and GET request handlers
    -Quality of life changes in the parsing of the queries
    -Changed orthographical errors
    -Added -r -s -d options for the server execution (resume, start, drop)
    -Created functions to handle GET and POST requests
    -Informative prints added for better understanding of the actions of the server
    -FIXED: made database cursor a global variable or else couldn't execute code outside main

--- src/servertest.py
+++ src/servertest.py
@@ -5,32 +5,77 @@ import cx_Oracle
 import time
 import json
 import cgi
-import testcode
+import secrets
 
 hostName = "localhost"
 serverPort = 9000
+cursor = None
 
-def get_querrys(sqlfile):
-    parsed_querries = []
-    parent_dir = __file__.strip("src\servertest.py") + '\querrys/'
+users = {}
+
+def app_id_generator():
+    return secrets.token_hex(16)
+
+def get_queries(sqlfile):
+    parsed_queries = []
+    parent_dir = __file__.strip("src\servertest.py") + '\queries/'
     for root, dirs, files in os.walk(parent_dir):
         if sqlfile in files:
             directory = root+"/"+sqlfile
             with open(directory) as file:
-                querries = file.read()
-                querries = querries.split(';')
-                querries_copy = list(querries)
-                for querry in querries_copy:
+                queries = file.read()
+                queries = queries.split(';')
+                queries_copy = list(queries)
+                for querry in queries_copy:
                     q = querry.splitlines()
                     q_copy = list(q)
                     for line in q_copy:
                         if line.startswith('--') or line.startswith('\n--'):
                             q.remove(line);
                     q = ''.join(map(str,q))
-                    parsed_querries.append(q)
-                parsed_querries.remove('')
+                    parsed_queries.append(q)
+                parsed_queries.remove('')
          
-    return parsed_querries
+    return parsed_queries
+
+def execute_queries(queries):
+    error_flag = False
+    results = []
+    for querry in queries:
+        try:
+            cursor.execute(querry)
+            results.append(cursor.fetchall())
+        except cx_Oracle.DatabaseError as ex:
+            error_flag = True
+            results.append("ERROR")
+        except cx_Oracle.InterfaceError:
+            pass
+    return results, error_flag
+
+def handle_get_request(message:dict):
+    if message.get("appid") == "request":
+        print("App Id requested")
+        print("Served Id:\n",app_id_generator())
+    if message.get("appid") != "request" or not None:
+        pass
+    print("\n") 
+
+
+def handle_post_request(message:dict):
+    if message.get("appid") != "request" or not None:
+        if message.get("action") == "login":
+            print("Login requested")
+            try:
+                assert len(message.get("args")) == 2
+            except AssertionError:
+                print("Insuffiecient login Data")
+            if True:
+                #something
+                print("Login successful")
+                users[message.get("appid")] = 1
+                print(users)
+    print("\n")           
+
 
 class MyServer(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -40,8 +85,6 @@ class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write(bytes(json.dumps({'hello': 'world', 'received': 'ok'}), encoding='utf8'))
-        testcode.outsidePrint()
         length = 0
         try:
             length = int(self.headers['Content-Length'])
@@ -49,9 +92,18 @@ class MyServer(BaseHTTPRequestHandler):
             pass
         if length:
             message = json.loads(self.rfile.read(length))
-            print(message)
-        else:
-            print("No Body")
+            handle_get_request(message)
+    
+    def do_POST(self):
+        self._set_headers()
+        length = 0
+        try:
+            length = int(self.headers['Content-Length'])
+        except:
+            pass
+        if length:
+            message = json.loads(self.rfile.read(length))
+            handle_post_request(message)
 
 
 if __name__ == "__main__":
@@ -66,33 +118,43 @@ if __name__ == "__main__":
         webServer = HTTPServer((hostName, serverPort), MyServer)
         username = sys.argv[2]
         password = sys.argv[3]
-        #username = "mde_trb1"
-        #password = "mde"
         
         try:
+            print("Connecting to Database...\n")
             connectstr = username+"/"+password+"@//localhost:1521/xe"
             conn = cx_Oracle.connect(connectstr)
         
         except cx_Oracle.DatabaseError:
             print("\nDATABASE ERROR: Username/Password not correct.\n")
             exit()
-        
-        print("\nConnected to the database")
-        print("Server started http://%s:%s\n" % (hostName, serverPort))
-        
+
         cursor = conn.cursor()
-        test_querrys = get_querrys("drop_all.sql")
-        #print(test_querrys)
-        results = []
-        for querry in test_querrys:
-            try:
-                cursor.execute(querry)
-                results.append(cursor.fetchall())
-            except cx_Oracle.DatabaseError:
-                print("\n\nERROR\n\n",querry)
-            except cx_Oracle.InterfaceError:
-                pass
-        print(results)
+        print("\nConnected successfully\n")
+
+        if sys.argv[1] == '-s':
+            print("\nStarting or Rebuilding Database\n")
+            result, error = execute_queries(get_queries("drop_all.sql"))
+            if not error:
+                print("Tables Dropped")
+            result, error = execute_queries(get_queries("fitness_schema.sql"))
+            if not error:
+                print("Database Build Successful")
+        
+        if sys.argv[1] == '-r':
+            result, error = execute_queries(["select * from fit_user"])
+            if not error:
+                print("\nReconnected to the Database\n")
+            else:
+                print("\nDatabase Empty")
+                print("Exiting...")
+                exit()
+        
+        if sys.argv[1] == '-d':
+            result, error = execute_queries(get_queries("drop_all.sql"))
+            print("Database Dropped")
+
+        print("Server started http://%s:%s\n" % (hostName, serverPort))
+
         try:
             webServer.serve_forever()
         except KeyboardInterrupt:
@@ -103,5 +165,5 @@ if __name__ == "__main__":
     
     else:
         print("\nNo arguments detected\n")
-        print("Use option -s to start database or -r to resume database\n")
+        print("Use option -s to start database -r to resume database or -d to drop database\n")
         print("After the option insert username and password to the database\n")
