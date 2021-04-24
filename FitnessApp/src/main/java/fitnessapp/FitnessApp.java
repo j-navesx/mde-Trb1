76013fd14887288a52f509d34b0747a5aa4f8335
j-navesx <j.naves@campus.fct.unl.pt>
2021-04-24 09:53:20 +0100

    Started the user interface
    -BUG: Program doesn't run with the image (have to redo the label)

--- FitnessApp/src/main/java/fitnessapp/FitnessApp.java
+++ FitnessApp/src/main/java/fitnessapp/FitnessApp.java
@@ -15,6 +15,6 @@ public class FitnessApp {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        
     }
 }
