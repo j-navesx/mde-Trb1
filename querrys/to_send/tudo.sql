fd14568cae52e0c51474fd0cafd6acd9b54d511b
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-05-02 19:22:07 +0100

    klbjdkljbfvdskjblhfds

--- querrys/to_send/tudo.sql
+++ querrys/to_send/tudo.sql
@@ -847,6 +847,7 @@ end;
         full outer join exercises on exercises.ex_id = notice.exercises_ex_id
         full outer join activities_template on activities_template.id = exercises.activities_template_id  
     where activities_template.id = (select activities_template_id from exercises where exercises.ex_id = notice.exercises_ex_id)
+    and notice.date_hour between sysdate-7 and sysdate
     order by notice.date_hour desc
 ;
 --------------------------------------------------------
