9df0253a814269f14e042573d9a13f479e1df412
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-05-02 18:47:25 +0100

    grtsdsfghbdgfshd

--- querrys/to_send/tudo.sql
+++ querrys/to_send/tudo.sql
@@ -879,6 +879,7 @@ end;
         to_char(transaction.t_date,'YYYY-MM-DD') as t_date
     from transaction
         left join profile on profile.fit_user_id = transaction.fit_user_id
+        where transaction.t_date between sysdate-7 and sysdate
         order by transaction.t_date desc
 ;
 --------------------------------------------------------
