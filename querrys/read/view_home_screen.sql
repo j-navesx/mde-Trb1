473a40c565c6add87b753b115678b980e4f7e004
g-pombo <45361838+g-pombo@users.noreply.github.com>
2021-05-02 14:33:46 +0100

    home_screen bug fix

--- querrys/read/view_home_screen.sql
+++ querrys/read/view_home_screen.sql
@@ -3,7 +3,7 @@ create or replace view home_screen
     select 
         profile.fit_user_id,
         profile.name, 
-        daily_status.steps, 
+        daily_status.steps,
         daily_goals.daily_steps, 
         profile.weight,
         extract(day from 24*60*exercises.duration) as duration, 
@@ -11,7 +11,7 @@ create or replace view home_screen
         exercises.calories, 
         to_char(exercises.begin_date,'yyyy-mm-dd') begin_date
         from profile
-        full outer join daily_status on daily_status.fit_user_id = profile.fit_user_id
-        full outer join daily_goals on daily_goals.fit_user_id = daily_status.fit_user_id
-        full outer join exercises on exercises.fit_user_id = daily_goals.fit_user_id
+        left join daily_status on daily_status.fit_user_id = profile.fit_user_id
+        left join daily_goals on daily_goals.fit_user_id = profile.fit_user_id
+        left join exercises on exercises.fit_user_id = profile.fit_user_id
     where exercises.ex_id = (select max(ex_id) from exercises where fit_user_id = profile.fit_user_id) or exercises.ex_id is null
--- querrys/to_send/DML.sql
+++ querrys/to_send/DML.sql
@@ -514,7 +514,7 @@ end;
   select 
         profile.fit_user_id,
         profile.name, 
-        daily_status.steps, 
+        daily_status.steps,
         daily_goals.daily_steps, 
         profile.weight,
         extract(day from 24*60*exercises.duration) as duration, 
@@ -522,9 +522,9 @@ end;
         exercises.calories, 
         to_char(exercises.begin_date,'yyyy-mm-dd') begin_date
         from profile
-        full outer join daily_status on daily_status.fit_user_id = profile.fit_user_id
-        full outer join daily_goals on daily_goals.fit_user_id = daily_status.fit_user_id
-        full outer join exercises on exercises.fit_user_id = daily_goals.fit_user_id
+        left join daily_status on daily_status.fit_user_id = profile.fit_user_id
+        left join daily_goals on daily_goals.fit_user_id = profile.fit_user_id
+        left join exercises on exercises.fit_user_id = profile.fit_user_id
     where exercises.ex_id = (select max(ex_id) from exercises where fit_user_id = profile.fit_user_id) or exercises.ex_id is null
 ;
 --------------------------------------------------------
